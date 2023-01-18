e162b8cead0d4affdc44de08ac22a722576fc274
akmatove <ermek_akmatov@inbox.ru>
2023-01-18 13:06:24 +0600

    * Try 1

--- android/src/main/kotlin/com/example/application_info_plus/ApplicationInfoPlusPlugin.kt
+++ android/src/main/kotlin/com/example/application_info_plus/ApplicationInfoPlusPlugin.kt
@@ -17,10 +17,17 @@ class ApplicationInfoPlusPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private var applicationContext: Context? = null
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    applicationContext = binding.applicationContext
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "application_info_plus")
     channel.setMethodCallHandler(this)
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    applicationContext = null
+    channel.setMethodCallHandler(null)
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
@@ -31,7 +38,7 @@ class ApplicationInfoPlusPlugin: FlutterPlugin, MethodCallHandler {
 //      val info = packageManager.getPackageInfo(applicationContext!!.packageName, 0)
 //
 //      info.applicationInfo
-
+      val packageManager = applicationContext!!.packageManager
       val info= packageManager.getPackageInfo(applicationContext!!.packageName, 0)
       if (info.applicationInfo.flags and ApplicationInfo.FLAG_ALLOW_BACKUP != 0) {
         result.success("true")
@@ -43,9 +50,5 @@ class ApplicationInfoPlusPlugin: FlutterPlugin, MethodCallHandler {
     else {
       result.notImplemented()
     }
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
   }
 }
